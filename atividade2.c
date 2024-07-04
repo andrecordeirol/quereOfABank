72b599a8d0a7f3b21eda6007912323d8aa37e392
andrecordeirol <andrecordeirolacerda@gmail.com>
2024-07-04 16:39:21 -0300

    new funtion

--- atividade2.c
+++ atividade2.c
@@ -1,61 +1,110 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 #define size 10
 
-struct pclient {
+struct pclient
+{
     char name[50];
-    int time;
+    char time[6];
 };
 
-struct pstack {
+struct pquere
+{
     struct pclient data[size];
     int start;
     int end;
 };
 
-struct pstack stack;
-
+//Global variables
+struct pquere fila;
 int start = 0;
 int end = 0;
 
-void quere() {
-    if (stack.end == size) {
+//Prototipation
+void quere();
+void unquere();
+void show_quere();
+void show_menu1();
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    int op = 1;
+    while (op != 0)
+    {
+        system("cls");
+        show_menu1();
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+            quere();
+            break;
+        case 2:
+            unquere();
+            break;
+        case 3:
+            show_quere();
+            break;
+        }
+    }
+    return (0);
+}
+
+void quere()
+{
+    if (fila.end == size)
+    {
         printf("The quere is full, wait outside.\n\n");
         system("pause");
     }
-    else {
+    else
+    {
         printf("Enter your name:");
-        scanf("%s", stack.data[stack.end].name);
-        time_t tempo = time(NULL);
-        //time = &tempo;
-        stack.data[stack.end].time = %tempo;
-        //strcpy(stack.data[stack.end].time, "time");
-        stack.end++;
+        scanf("%s", fila.data[fila.end].name);
+        printf("Enter the time in that client came:");
+        scanf("%s", fila.data[fila.end].time);
+        fila.end++;
     }
 }
 
-void unquere() {
-    if(stack.end == stack.start) {
+void unquere()
+{
+    if (fila.end == fila.start)
+    {
         printf("The quere is empty.\n");
         system("pause");
     }
-    else {
-
-        for (int i = 0;i<size; i++) {
-            strcpy(stack.data[i].name, stack.data[stack.start+1].name);
-            stack.data[i] = stack.data[stack.start+1];
-            stack.end--;
+    else
+    {
+        for (int i=0; i< size; i++)
+        {
+            strcpy(fila.data[i].name, fila.data[i + 1].name);
+            strcpy(fila.data[i].time, fila.data[i + 1].time);
+            fila.end--;
         }
     }
 }
 
-void show_quere() {
-    printf("[");
-    for (int i=0;i<size;i++) {
-        printf("%s, ", stack.data[i].name);
+void show_quere()
+{
+    printf("");
+    for (int i = 0; i < size; i++)
+    {
+        printf("Cliente %s,", fila.data[i].name);
+        printf("came at %s time \n", fila.data[i].time);
     }
     printf("]\n\n");
+}
+
+void show_menu1()
+{
+    printf("Choole a alternative\n\n");
+    printf("1 - Queue in a value\n");
+    printf("2 - Unqueue out a value\n");
+    printf("3 - Show queue\n");
+    printf("0 - Quit\n");
 }
